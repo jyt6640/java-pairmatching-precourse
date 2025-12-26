2c562dee46984b9826be87b00355d57e108780c4
jyt6640 <jyt6640@gmail.com>
2025-12-26 17:08:47 +0900

    test(MatchingHistoryTest): 매칭 이력이 있는지 조회하는 테스트 추가

--- src/test/java/pairmatching/domain/MatchingHistoryTest.java
+++ src/test/java/pairmatching/domain/MatchingHistoryTest.java
@@ -1,6 +1,7 @@
 package pairmatching.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,9 +20,9 @@ public class MatchingHistoryTest {
 
     @DisplayName("매칭 결과를 저장")
     @Test
-    void 매칭_결과_저장() {
+    void 매칭_결과를_저장() {
         //given
-        List<Pair> pairs = Arrays.asList(new Pair("용태", "수미"));
+        List<Pair> pairs = List.of(new Pair("용태", "수미"));
         PairMatchingResult result = new PairMatchingResult(
                 Course.BACKEND, Level.LEVEL1, "자동차경주", pairs
         );
@@ -31,4 +32,18 @@ public class MatchingHistoryTest {
                 .doesNotThrowAnyException();
     }
 
+    @DisplayName("매칭 이력이 있는지 조회")
+    @Test
+    void 매칭_이력이_있는지_조회() {
+        //given
+        List<Pair> pairs = List.of(new Pair("용태", "수미"));
+        PairMatchingResult result = new PairMatchingResult(
+                Course.BACKEND, Level.LEVEL1, "자동차경주", pairs
+        );
+        history.add(result);
+
+        //when&then
+        assertThat(history.exists(Course.BACKEND, Level.LEVEL1, "자동차경주")).isTrue();
+        assertThat(history.exists(Course.BACKEND, Level.LEVEL1, "로또")).isFalse();
+    }
 }
