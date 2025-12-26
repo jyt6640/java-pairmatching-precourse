7cdf65327a901f759bb39fd5475b53a60ae17cac
jyt6640 <jyt6640@gmail.com>
2025-12-26 14:36:46 +0900

    feat(Crew): 배열로 Crew 객체 생성 기능 구현
    - gradle 버전 변경

---
+++ src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class Crew {
+
+    private List<String> crewNames;
+
+    public Crew(List<String> crewNames) {
+        this.crewNames = crewNames;
+    }
+}
