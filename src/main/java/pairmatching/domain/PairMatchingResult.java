ddf69032d73e8769818c53958c2d0c8609dcabab
jyt6640 <jyt6640@gmail.com>
2025-12-26 16:41:56 +0900

    feat(PairMatchingResult): 코스, 레벨, 미션 정보를 가진 매칭 결과 객체 생성 기능 구현

---
+++ src/main/java/pairmatching/domain/PairMatchingResult.java
@@ -0,0 +1,18 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+public class PairMatchingResult {
+
+    private Course course;
+    private Level level;
+    private String mission;
+    private List<Pair> pairs;
+
+    public PairMatchingResult(Course course, Level level, String mission, List<Pair> pairs) {
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+        this.pairs = pairs;
+    }
+}
